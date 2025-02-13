e9766b04a6ec6666cbdb88304915d6df552d3f06
William O <williamobrien2001@gmail.com>
2025-02-13 17:35:40 +0000

    Spring Social Media Blog API complete

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,77 @@
 package com.example.service;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class AccountService {
+    AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+    public Account addAccount(Account account){
+        if(doesNotExist(account) != null || account.getPassword().length() < 4){
+            return null;
+        }
+        return accountRepository.save(account);
+    }
+
+    public Account login(Account account){
+        Account a = doesNotExist(account);
+        if(a == null){
+            return null;
+        }
+        if(a.getPassword().equals(account.getPassword())){
+            return a;
+        }
+        return null;
+    }
+
+    public boolean checkDuplicate(Account account){
+        List<Account> accounts = accountRepository.findAll();
+
+        for(Account a : accounts){
+            if(a.getUsername().equals(account.getUsername())){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Account doesNotExist(Account account){
+        if(account.getUsername().equals("")){
+            return null;
+        }
+        List<Account> accounts = accountRepository.findAll();
+
+        for(Account a : accounts){
+            if(a.getUsername().equals(account.getUsername())){
+                return a;
+            }
+        }
+
+        return null;
+    }
+
+    public Boolean checkId(Integer id){
+        List<Account> accounts = accountRepository.findAll();
+        for(Account a : accounts){
+            if(a.getAccountId().equals(id)){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,77 @@
 package com.example.service;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Message;
+import com.example.repository.MessageRepository;
+
+import java.util.List;
+import java.util.ArrayList;
+
+@Service
+@Transactional
 public class MessageService {
+    MessageRepository messageRepository;
+
+    @Autowired
+    public MessageService(MessageRepository messageRepository){
+        this.messageRepository = messageRepository;
+    }
+
+    public Message addMessage(Message message){
+        if(message.getMessageText().equals("") || message.getMessageText().length() > 255){
+            return null;
+        }
+        return messageRepository.save(message);
+    }
+
+    public List<Message> getMessages(){
+        return messageRepository.findAll();
+    }
+
+    public Message getMessageByID(Integer id){
+        if(messageRepository.existsById(id)){
+            List<Message> messages = messageRepository.findAll();
+            for(Message m : messages){
+                if(m.getMessageId().equals(id)){
+                    return m;
+                }
+            }
+        }
+        return null;
+    }
+
+    public Integer deleteByID(int id){
+        if(messageRepository.existsById(id)){
+            messageRepository.deleteById(id);
+            return 1;
+        }
+        return null;
+    }
+
+    public int updateMessage(String messageText, int id){
+        if(messageRepository.existsById(id) && !messageText.equals("") && messageText.length() < 255){
+            Message message = messageRepository.getById(id);
+            message.setMessageText(messageText);
+            messageRepository.save(message);
+            return 1;
+        }
+        return 0;
+    }
+
+    public List<Message> getMessagesByUser(Integer postedBy){
+        List<Message> messages = messageRepository.findAll();
+        List<Message> userMessages = new ArrayList<Message>();
+
+        for(Message m : messages){
+            if(m.getPostedBy().equals(postedBy)){
+                userMessages.add(m);
+            }
+        }
+
+        return userMessages;
+    }
 }
